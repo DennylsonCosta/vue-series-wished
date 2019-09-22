0bbe94fb5c01fa0b06e8abb1ae232ade4de6c79c
Dennylson Costa <dennylsonpdr@gmail.com>
2019-09-22 18:28:45 -0300

    Class 04

---
+++ src/pages/watchlist/Watchlist.vue
@@ -0,0 +1,20 @@
+<template>
+<h1>Watchlist</h1>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex'
+
+export default {
+  mounted() {
+    this.ActionFindWatchlist()
+  },
+  methods: {
+    ...mapActions('watchlist', ['ActionFindWatchlist'])
+  }
+}
+</script>
+
+<style>
+
+</style>
