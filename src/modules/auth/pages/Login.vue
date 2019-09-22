f3a3dc9109c7682960ec645d222139b2b6d9a1ba
Dennylson Costa <dennylsonpdr@gmail.com>
2019-09-22 11:53:34 -0300

    Class 02

--- src/modules/auth/pages/Login.vue
+++ src/modules/auth/pages/Login.vue
@@ -1,15 +1,71 @@
 <template>
-  <div>
-    <h1>Login</h1>
-  </div>
+  <form @submit.prevent="submit()">
+    <div class="login-page">
+      <div class="card">
+        <div class="card-header">Login</div>
+        <div class="card-body">
+          <div class="form-group">
+            <input
+              required
+              type="email"
+              class="form-control"
+              placeholder="E-mail"
+              v-model="form.email"
+            >
+          </div>
+          <div class="form-group">
+            <input
+              required
+              type="password"
+              class="form-control"
+              placeholder="Senha"
+              v-model="form.password"
+            >
+          </div>
+
+          <button class="btn btn-primary w-100k">
+            Entrar
+          </button>
+        </div>
+      </div>
+    </div>
+  </form>
 </template>
 
 <script>
-export default {
+import { mapActions } from 'vuex'
 
+export default {
+  data: () => ({
+    form: {
+      email: '',
+      password: ''
+    }
+  }),
+
+  methods: {
+    ...mapActions('auth', ['ActionDoLogin']),
+    async submit () {
+      try {
+        await this.ActionDoLogin(this.form)
+
+        this.$router.push({ name: 'home' })
+      } catch (err) {
+        console.error(err.data ? err.data.message : 'Não foi possivel fazer o login')
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped lang="scss">
+.login-page {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .card {
+    width: 30%;
+  }
+}
 </style>
